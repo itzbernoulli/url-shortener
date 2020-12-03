bfa52e89f5be27c4e070bf136cb2580bd3ec0620
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-03 08:43:39 +0100

    add model resource

---
+++ app/models/link.rb
@@ -0,0 +1,6 @@
+class Link < ApplicationRecord
+    validates_presence_of :url
+    validates :url, format: URI::regexp(%w[http https])
+    validates_uniqueness_of :slug
+    validates_length_of :url, within: 3..255, on: :create, message: "too long"
+end
