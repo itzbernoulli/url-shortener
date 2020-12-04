9f64b2dda0e24e92b483b17d7a80239243525274
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-04 03:28:54 +0100

    add best_hundred resources

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,10 @@
 class LinksController < ApplicationController
     def index
-        @links = Link.order(clicked: :desc).limit(100)
+        @links = Link.all.paginate(page: params[:page], per_page: 30)
+    end
+
+    def best_hundred
+        @links = Link.order(clicked: :desc).limit(100).paginate(page: params[:page], per_page: 30)
     end
 
     def new
@@ -11,12 +15,19 @@ class LinksController < ApplicationController
         @link = Link.new(link_params)
         @link.url = @link.sanitize
         if @link.save
-            # ScraperJob.perform_later(@link.id)
+            ScraperJob.perform_later(@link.id)
             redirect_to links_path
         else
             flash[:error] = @link.errors.full_messages
             redirect_to new_link_path
         end
+    end
+
+    def show
+        @link = Link.find_by_slug(params[:slug])
+        return render file: 'public/404.html', status: :not_found, layout: false if @link.nil?
+        @link.update_attribute(:clicked, @link.clicked + 1)
+        redirect_to @link.url
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'links#index'
-  get '/s/:slug', to: 'links#show', as: :short
+  get '/:slug', to: 'links#show', as: :short
 
-  resources :links, only: [:create, :new, :index]
+ 
+  resources :links, only: [:create, :new, :index] do
+    collection do
+      get :best_hundred
+    end
+  end
 end
