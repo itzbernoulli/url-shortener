ffc8088ac2961d349a748cf34483a44df96b43ba
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-04 06:46:53 +0100

    disable case sensitivity on uniqueness matcher

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
   context 'validations' do
     it { should validate_presence_of(:url) }
-    it { should validate_uniqueness_of(:slug) }
+    it { should validate_uniqueness_of(:slug).ignoring_case_sensitivity }
     it { should validate_length_of(:url) }
     it { should allow_values('http://foo.com', 'https://bar.com').for(:url)}
     it { should_not allow_values('foo.com', 'bar.com').for(:url)}
