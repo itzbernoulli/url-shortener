82e6dd1e5173db52b1b67b66257ba5ac9fb67b20
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-04 04:38:16 +0100

    modify route order to consider slug last

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'links#index'
-  get '/:slug', to: 'links#show', as: :short
-
- 
+  
   resources :links, only: [:create, :new, :index] do
     collection do
       get :best_hundred
     end
   end
+  get '/:slug', to: 'links#show', as: :short
 end
