c6897b5283daebbb3e5600919e52df27382b60b3
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-08 05:46:22 +0100

    refactor job to use url and slug

--- app/jobs/scraper_job.rb
+++ app/jobs/scraper_job.rb
@@ -4,16 +4,21 @@ class ScraperJob < ApplicationJob
     require 'open-uri'
     require 'nokogiri'
   
-    def perform(link_id)
+    def perform(url,slug)
+      p '*************'
+      p url
+      p slug
       begin
-        link = Link.find_by_id(link_id)
-        html = URI.open(link.url)
+        # link = Link.find_by(:id, link_id)
+        # p link
+        html = URI.open(url)
       
         doc = Nokogiri::HTML(html)
         title = doc.css('head title').text
-        link.update_attribute(:title, title)
+        p title
+        Link.find_by_slug(slug).update_column(:title, title)
       rescue => exception
-        link.update_attribute(:title, "Error recovering title")
+        Link.find_by_slug(slug).update_column(:title, "Error recovering title")
       end
 
     end
