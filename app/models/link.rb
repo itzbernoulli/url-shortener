c180d08846f019a9ab88b30d84c30f7df687bb3c
Ayodeji <oyinloyeayodeji@gmail.com>
2020-12-08 05:44:31 +0100

    generate link with bijective function

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,11 +4,27 @@ class Link < ApplicationRecord
     validates_uniqueness_of :slug
     validates_length_of :url, within: 3..255, on: :create, message: "too long"
 
-    before_validation :generate_slug
+    # before_validation :generate_slug
     before_save :sanitize
 
+    after_save :generate_slug
+    after_commit :retrieve_title
+
+    def shortened_url
+        ENV['DOMAIN'] + self.slug
+    end
+
+    private
+    ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
+
     def generate_slug
-        self.slug = SecureRandom.uuid[0..5] if self.slug.nil? || self.slug.empty?
+        s = ''
+        base = ALPHABET.length
+        while self.id > 0
+          s << ALPHABET[self.id.modulo(base)]
+          self.id /= base
+        end
+        self.update_column(:slug, s.reverse)
     end
 
     def sanitize
@@ -17,7 +33,7 @@ class Link < ApplicationRecord
         sanitize_url = self.url.downcase.gsub(/(www\.)/,"")
     end
 
-    def shortened_url
-        ENV['DOMAIN'] + self.slug
+    def retrieve_title
+        ScraperJob.perform_later(self.url,self.slug)
     end
 end
